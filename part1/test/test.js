34232b718407f1dcd9a9e94888eec7f717b5f064
wlmrh <13611907701@163.com>
2025-08-25 14:08:42 +0800

    part1 example

--- part1/test/test.js
+++ part1/test/test.js
@@ -1,13 +1,6 @@
-const t = [[1, 2], [3, 4], [5, 6]]
+// Function returning function
+const add = (x) => (y) => x + y;
+// Function accepting function
+const babies = ["dog", "cat", "hen"].map((name) => `baby ${name}`);
 
-const modify = t.map((a, b)=>{
-    a += 2
-    b += 1
-    // console.log(a)
-    console.log(b)
-    return (b, a)
-})
-
-modify.forEach(value=>{
-    console.log(value)
-})
+babies.forEach(val => console.log(val))
